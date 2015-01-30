b1139aba096487da1a076d8cd77e19bf590d2a0f
Victor Logunov <v.logunov@advmaker.net>
2015-01-30 19:09:48 +0300

    update

---
+++ objects/main.js
@@ -0,0 +1,15 @@
+/**
+ * Created by vic on 23.01.15.
+ */
+
+var Vector = function (x , y) {
+    this.x = x;
+    this.y = y;
+}
+
+var ql = new Vector(2 , 2);
+console.log( ql );
+console.log(ql.x);
+
+
+console.log(self.parent);
